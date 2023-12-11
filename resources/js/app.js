e8e7776bc813dd84b90983a8fdf8d0d49528b19f
Chong Jin Yi <chong601.13@gmail.com>
2023-12-11 21:26:39 +0800

    Fix stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,16 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import * as dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+window.dayjs = dayjs;
 window.Alpine = Alpine;
 
 Alpine.start();
